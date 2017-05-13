779994fee5fed0e55589e3f67e96d7a276db6394
smg <audrl1010@naver.com>
2017-05-13 22:45:20 +0900

    Initial Commit

---
+++ KaKaoChatInputView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KaKaoChatInputView
+//
+//  Created by smg on 2017. 5. 13..
+//  Copyright © 2017년 grutech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
